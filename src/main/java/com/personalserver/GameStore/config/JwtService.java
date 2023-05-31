c19a8eb1986f2954f28751fc70cb43d35c2893b2
aisha-png <67974517+aisha-png@users.noreply.github.com>
2023-05-31 23:21:25 +0100

    fixed bug in jwt service

--- src/main/java/com/personalserver/GameStore/config/JwtService.java
+++ src/main/java/com/personalserver/GameStore/config/JwtService.java
@@ -47,7 +47,7 @@ public class JwtService {
     // is token valid or expired:
     public boolean isTokenValid(String token, UserDetails userDetails){
         final String username = extractUsername(token);
-        return(username.equals(userDetails.getUsername())) && !isTokenValid(token);
+        return(username.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
     private boolean isTokenExpired(String token){
         return extractExpiration(token).before(new Date());
