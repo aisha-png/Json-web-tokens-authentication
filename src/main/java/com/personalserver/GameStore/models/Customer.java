dfdadef8725a856468b8150c286cd44c049d7a54
aisha-png <67974517+aisha-png@users.noreply.github.com>
2023-05-31 21:58:43 +0100

    connected to postgresql db

---
+++ src/main/java/com/personalserver/GameStore/models/Customer.java
@@ -0,0 +1,25 @@
+package com.personalserver.GameStore.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
---
+++ src/test/java/com/personalserver/GameStore/GameStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalserver.GameStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
