fa2ccecd707e91bb74433ec33b5315a797ab8fc7
aisha-png <67974517+aisha-png@users.noreply.github.com>
2023-06-01 20:58:52 +0100

    completed authentication provider and pw encoder

--- src/main/java/com/personalserver/GameStore/config/ApplicationConfiguration.java
+++ src/main/java/com/personalserver/GameStore/config/ApplicationConfiguration.java
@@ -4,8 +4,12 @@ import com.personalserver.GameStore.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -17,5 +21,20 @@ public class ApplicationConfiguration {
     public UserDetailsService userDetailsService(){
         return username -> userRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        // has multiple implementations
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        // specify properties - user detail service - from db, from in-memory db, ldap
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
